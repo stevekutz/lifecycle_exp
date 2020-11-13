36343eaf33720f03759850b7352cff39ca3e6595
stevekutz <stkutz@gmail.com>
2020-11-12 20:08:47 -0600

    set up components to render API data

---
+++ app/src/components/User.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import classes from './user.module.css';
+
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "shards-ui/dist/css/shards.min.css";
+import {Alert,   
+    Card,
+    CardHeader,
+    CardTitle,
+    CardImg,
+    CardBody,
+    CardFooter,
+    Container
+    } from 'shards-react';
+
+
+const user = (props) => {
+
+    console.log('User props', props);
+
+    return (
+        <React.Fragment>
+        <div className = {classes.UserContainer}>
+            <Card >
+                <CardHeader> {props.user.id}</CardHeader>
+                <CardTitle> {props.user.name}</CardTitle>
+                <CardBody>
+                    <p> username: {props.user.username} </p>
+                    <p> email: {props.user.email} </p>
+                    
+                
+                
+                </CardBody>
+            
+            </Card>
+        
+        
+        
+        
+        
+        </div>
+
+        
+        
+        </React.Fragment>
+    )
+
+
+}
+
+export default user;
---
+++ app/src/components/Users.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import User from './User';
+import dataUsers from '../data/dataUsers';
+
+import classes from './users.module.css';
+
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "shards-ui/dist/css/shards.min.css";
+import {
+    Container
+    } from 'shards-react';
+
+
+
+const Users = (props) => {
+
+    console.log(dataUsers);
+
+
+    return (
+    
+        <Container className = {classes.Container}>
+
+        {dataUsers.map((user, key) => {
+            return (
+                <User 
+                    key = {user.id}
+                    user = {user}
+                />    
+            )
+        
+        
+        })}
+
+        
+        
+        
+        </Container>
+        
+    
+    )
+
+
+
+}
+
+export default Users;
+
+// 0	
+//  id	1
+//  name	"Leanne Graham"
+//  username	"Bret"
+//  email	"Sincere@april.biz"
+//  address	
+//      street	"Kulas Light"
+//      suite	"Apt. 556"
+//      city	"Gwenborough"
+//      zipcode	"92998-3874"
+// geo	
+//  lat	"-37.3159"
+//  lng	"81.1496"
+// phone	"1-770-736-8031 x56442"
+// website	"hildegard.org"
+// company	
+//  name	"Romaguera-Crona"
+//  catchPhrase	"Multi-layered client-server neural-net"
+//  bs	"harness real-time e-markets"
